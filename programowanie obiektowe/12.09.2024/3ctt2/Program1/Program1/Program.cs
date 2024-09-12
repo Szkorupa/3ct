07a9194dd94d9b2b4e504b6d13cab1121d44cf7e
Programista <maksszkorupa@interia.pl>
2024-09-12 16:57:42 +0200

    12.09.2024

---
+++ programowanie obiektowe/12.09.2024/3ctt2/Program1/Program1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj 3 CT");
+            Console.WriteLine("Witaj 3 CT");
+
+            Console.Write("Witaj 3CT" + Convert.ToChar(13) + Convert.ToChar(10));
+
+            while (true)
+            {
+                Console.Write(DateTime.Now.ToString() + Convert.ToChar(13));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
