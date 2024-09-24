8565e1af87ab91f483c9acacf775bcff45f7f759
MelaNaSpella Dev <sh3p4rdz1k@gmail.com>
2024-09-24 07:49:26 +0200

    New Branch

---
+++ GB/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("03648f75-0a7e-49ae-b1b4-9b5977190b8e")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ programowanie obiektowe/19.09.2024.BJ/ProgramHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal static class ProgramHelpers
+{
+    static void Main(string[] args)
+    {
+        int[] zmienna = new int[10];
+
+        for (int i = 0; i <= 10; i++)
+        {
+            zmienna[1] = i;
+            Console.WriteLine(zmienna[i].ToString());
+
+        }
+        Console.ReadKey();
+    }
+}
