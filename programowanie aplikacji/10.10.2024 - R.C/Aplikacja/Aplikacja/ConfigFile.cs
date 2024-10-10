5eb338e9a85130f6cd502a8ea990bc61f8dbab3b
Szkorupa <szkorupa@github.com>
2024-10-10 16:22:09 +0200

    17:20

---
+++ programowanie aplikacji/10.10.2024 - R.C/Aplikacja/Aplikacja/ConfigFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Aplikacja
+{
+    internal class ConfigFile
+    {
+            string Path;
+            string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+
+            static extern long GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int size, string FilePath);
+
+            public string Read(string Key, string Section = null)
+            {
+                var RetVal = new StringBuilder(255);
+                GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+                return RetVal.ToString();
+            }
+
+            public void Write(string Key, string Value, string Section = null)
+            {
+                WritePrivateProfileString(Section ?? EXE, Key ,Value ,Path); 
+            }
+
+            public void DeleteKey(string Key, string Section = null)
+            {
+                Write(Key, null, Section ?? EXE);
+            }
+
+            public void DeleteSection(string Section = null)
+            {
+                Write(null, null, Section ?? EXE);
+            }
+            public ConfigFile(string IniPath = null)
+            {
+                Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+            }
+    }
+}
---
+++ programowanie aplikacji/10.10.2024 - R.C/Aplikacja/Aplikacja/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Aplikacja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Ini = new ConfigFile(@"./config.ini");
+            string parameter_value;
+
+            parameter_value = Ini.Read("ipaddress", "printer");
+            Console.WriteLine("ip address = " + parameter_value);
+
+            parameter_value = Ini.Read("port", "printer");
+            Console.WriteLine("port = " + parameter_value);
+
+            string printer_name;
+            parameter_value = Ini.Read("printer_name", "Printer");
+            if (string.IsNullOrEmpty(parameter_value))
+            {
+                printer_name = "default_name";
+                Ini.Write("printer_name", printer_name, "Printer");
+            }
+            else if (!string.IsNullOrEmpty(parameter_value))
+            {
+                printer_name = parameter_value;
+            }
+
+            
+
+
+            var i = 1;
+            while (true)
+            {
+                Console.WriteLine("Lezy " + i + " skibidi");
+                i++;
+                Thread.Sleep(1000);
+
+                if (i == 34)
+                {
+                    Console.WriteLine("Za 34");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
